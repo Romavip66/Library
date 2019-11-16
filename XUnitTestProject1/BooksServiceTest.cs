e81b36e8dae2b28c093f9a12544ef20a55287ea9
Romavip66 <37201724+Romavip66@users.noreply.github.com>
2019-11-16 07:25:58 +0600

    Add files via upload

---
+++ XUnitTestProject1/BooksServiceTest.cs
@@ -0,0 +1,78 @@
+﻿using LibraryCourse.Abstractions;
+using LibraryCourse.Models;
+using LibraryCourse.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class BooksServiceTest
+    {
+        [Fact]
+        public async Task AddTest()
+        {
+            var fake = Mock.Of<IBooksR>();
+            var booksService = new BooksService(fake);
+
+            var books = new Books() { Book_Name = "add-name", Author_Name = "add-author" };
+            await booksService.AddAndSave(books);
+        }
+        [Fact]
+        public async Task UpdateTest()
+        {
+            var fake = Mock.Of<IBooksR>();
+            var booksService = new BooksService(fake);
+
+            var books = new Books() { Book_Name = "update-name", Author_Name="update-author" };
+            await booksService.Update(books);
+        }
+        [Fact]
+        public async Task RemoveTest()
+        {
+            var fake = Mock.Of<IBooksR>();
+            var booksService = new BooksService(fake);
+            var books = new Books() { Book_Name = "delete-name", Author_Name="delete-author" };
+            await booksService.Delete(books);
+        }
+        [Fact]
+        public async Task DetailTest()
+        {
+            var fake = Mock.Of<IBooksR>();
+            var booksService = new BooksService(fake);
+            var id = 2;
+            await booksService.DetailsBooks(id);
+        }
+        [Fact]
+        public async Task GetBooksTest()
+        {
+            var books = new List<Books>
+            {
+                new Books() { Book_Name = "super-patient", Author_Name="super-patient2" },
+                new Books() { Book_Name = "invalid-patient", Author_Name="invalid-patient2" },
+            };
+
+            var fakeRepositoryMock = new Mock<IBooksR>();
+            fakeRepositoryMock.Setup(x => x.GetAll()).ReturnsAsync(books);
+
+
+            var booksService = new BooksService(fakeRepositoryMock.Object);
+
+            var resultBookss = await booksService.GetBooks();
+
+            Assert.Collection(resultBookss, Books =>
+            {
+                Assert.Equal("super-patient", Books.Book_Name);
+                Assert.Equal("super-patient2", Books.Author_Name);
+            },
+            Books =>
+            {
+                Assert.Equal("invalid-patient", Books.Book_Name);
+                Assert.Equal("invalid-patient2", Books.Author_Name);
+            });
+        }
+    }
+}
---
+++ XUnitTestProject1/HistoryServiceTest.cs
@@ -0,0 +1,79 @@
+﻿using LibraryCourse.Abstractions;
+using LibraryCourse.Models;
+using LibraryCourse.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class HistoryServiceTest
+    {
+        [Fact]
+        public async Task AddTest()
+        {
+            var fake = Mock.Of<IHistoryR>();
+            var historyService = new HistoryService(fake);
+
+            var history = new History() { CardId = 1, BookId = 1};
+            await historyService.AddAndSave(history);
+        }
+        [Fact]
+        public async Task UpdateTest()
+        {
+            var fake = Mock.Of<IHistoryR>();
+            var historyService = new HistoryService(fake);
+
+            var history = new History() { CardId = 2, BookId =2};
+            await historyService.Update(history);
+        }
+        [Fact]
+        public async Task RemoveTest()
+        {
+            var fake = Mock.Of<IHistoryR>();
+            var historyService = new HistoryService(fake);
+            var history = new History() { CardId = 3, BookId= 3};
+            await historyService.Delete(history);
+        }
+        [Fact]
+        public async Task DetailTest()
+        {
+            var fake = Mock.Of<IHistoryR>();
+            var historyService = new HistoryService(fake);
+            var id = 2;
+            await historyService.DetailsHistory(id);
+        }
+        [Fact]
+        public async Task GetHistoryTest()
+        {
+            var history = new List<History>
+            {
+                new History() { CardId = 4, BookId=4 },
+                new History() { CardId=5, BookId=5 },
+        };
+
+            var fakeRepositoryMock = new Mock<IHistoryR>();
+            fakeRepositoryMock.Setup(x => x.GetAll()).ReturnsAsync(history);
+
+
+            var historyService = new HistoryService(fakeRepositoryMock.Object);
+
+            var resultHistory = await historyService.GetHistory();
+
+            Assert.Collection(resultHistory, history =>
+            {
+                Assert.Equal(4, history.CardId);
+                Assert.Equal(4, history.BookId);
+            },
+            history =>
+            {
+                Assert.Equal(5, history.CardId);
+                Assert.Equal(5, history.BookId);
+
+            });
+        }
+    }
+}
